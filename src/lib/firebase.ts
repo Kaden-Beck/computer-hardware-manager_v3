141fec98acbfe6f3dcf779b76daf932da57e2a99
Kaden-Beck <kadenb12@gmail.com>
2026-03-19 17:01:37 -0600

    - Added Firebase Auth and Appcheck Config - Added useAuth hook - Updated App.tsx (landing) to use Shadcn prompt login with Google - Dashboard Page is now guarded

---
+++ src/lib/firebase.ts
@@ -0,0 +1,34 @@
+import { initializeApp } from 'firebase/app';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+
+export const app = initializeApp(firebaseConfig);
+
+// Set up Appcheck with Recaptcha
+export const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LfjMZAsAAAAAFeZDMCR51GpYa7bwH-7F-B6520H'),
+  isTokenAutoRefreshEnabled: true,
+});
+
+// Setup Authentication Service
+export const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
+export const signOutUser = () => signOut(auth);
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,6 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import DashboardComponent from '../components/Dashboard';
+import { auth } from '@/lib/firebase';
 
 export const Route = createFileRoute('/dashboard')({
+  beforeLoad: () => {
+    if (!auth.currentUser) {
+      throw redirect({ to: '/' });
+    }
+  },
   component: DashboardComponent,
 });
